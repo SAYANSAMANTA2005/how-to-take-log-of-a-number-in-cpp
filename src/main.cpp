43d8b07838498b5385fb9e4922726a8e535803ab
root <root@localhost>
2024-10-01 19:41:45 +0530

    auto publish 1727791904909

---
+++ src/main.cpp
@@ -0,0 +1,9 @@
+#include <iostream>
+#include <cmath>
+
+int main() {
+    double number = 5.0;
+    double result = log(number);  // Natural log (base e)
+    std::cout << "Natural log of " << number << " is " << result << std::endl;
+    return 0;
+}
